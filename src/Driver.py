9100e94a862ded268f28ac900459a3c685b55e1a
Jason Urton <jason.urton@gmail.com>
2011-03-12 23:03:00 -0500

    Progress

--- src/Driver.py
+++ src/Driver.py
@@ -1,42 +1,22 @@
 import nltk
 from nltk.corpus import treebank
 
-index = 0
+test_set = [0,1,5,6,8,12,13,26,54,66,69,73,75,79,80,83,95,114,130,146,152,153]
+chunks = nltk.corpus.treebank_chunk.chunked_sents()
 
-trees = nltk.corpus.treebank_chunk
-chunks = trees.chunked_sents()
-test = chunks[0]
-
-corpus = treebank.tagged_sents()[index]
-#print corpus
-
-grammar2 = "NP: {<PRP$>?<CD>?<DT>?<JJ.*>*<NN.*>*<POS>?<NN.*>*<JJ>?<CD>?}"
+#grammar2 = "NP: {<PRP$>?<CD>?<DT>?<JJ.*>*<NN.*>*<POS>?<NN.*>*<JJ>?<CD>?}"
 grammar = r"""
     NP: {<NN.*><CD><JJ>?}
         {<CD><NN.*>*<CD>?<JJ>?}
         {<DT|PP\$>?<JJ|RB>*<NN.*>+<VBG>?<IN>?<JJ>*<NN.*>*}
         {}
 """
-grammar3 = ""
 
-#cp = nltk.RegexpParser(grammar)
-cp = nltk.RegexpChunkParser(grammar)
-result = cp.evaluate(test)
-print result
+cp = nltk.RegexpParser(grammar)
+result = cp.parse(treebank.tagged_sents()[test_set[0]])
+print "My parse: \n" + str(result) + "\n"
+print "Actual parse: \n" + str(chunks[test_set[0]])
 
-
-
-#print result
-#print type(nltk.corpus.treebank_chunk.chunked_sents())
-#print cp.evaluate(nltk.corpus.treebank_chunk.chunked_words())
-#print cp.evaluate(treebank.tagged_sents()[index])
-#print cp.evaluate() #insert gold standard here - a subset of nltk.corpus.treebank_chunk
-
-
-
-#print result
-#print treebank.tagged_sents()[index]
-#print result, "\n", treebank.parsed_sents()[index]
-#for sentence in corpus:
+eval = cp.evaluate(chunks[test_set[0]], grammar)
     
     
