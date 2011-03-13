3cf051a2c0e67e494a7ccb7ab2568791bbf9a8ad
Jason Urton <jason.urton@gmail.com>
2011-03-12 22:40:42 -0500

    Progress?

--- src/Driver.py
+++ src/Driver.py
@@ -3,7 +3,12 @@ from nltk.corpus import treebank
 
 index = 0
 
+trees = nltk.corpus.treebank_chunk
+chunks = trees.chunked_sents()
+test = chunks[0]
+
 corpus = treebank.tagged_sents()[index]
+#print corpus
 
 grammar2 = "NP: {<PRP$>?<CD>?<DT>?<JJ.*>*<NN.*>*<POS>?<NN.*>*<JJ>?<CD>?}"
 grammar = r"""
@@ -11,13 +16,20 @@ grammar = r"""
         {<CD><NN.*>*<CD>?<JJ>?}
         {<DT|PP\$>?<JJ|RB>*<NN.*>+<VBG>?<IN>?<JJ>*<NN.*>*}
         {}
-
 """
+grammar3 = ""
 
 #cp = nltk.RegexpParser(grammar)
 cp = nltk.RegexpChunkParser(grammar)
-result = cp.parse(corpus)
+result = cp.evaluate(test)
 print result
+
+
+
+#print result
+#print type(nltk.corpus.treebank_chunk.chunked_sents())
+#print cp.evaluate(nltk.corpus.treebank_chunk.chunked_words())
+#print cp.evaluate(treebank.tagged_sents()[index])
 #print cp.evaluate() #insert gold standard here - a subset of nltk.corpus.treebank_chunk
 
 
