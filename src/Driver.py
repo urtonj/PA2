392bbb094cdd5825c0a84f4854416b665a2c5ffd
Jason Urton <jason.urton@gmail.com>
2011-03-16 15:11:02 -0400

    Complete

--- src/Driver.py
+++ src/Driver.py
@@ -1,31 +1,30 @@
 import nltk
-from nltk.corpus import treebank
 
-test_set = ['wsj_0001.pos', 'wsj_0002.pos', 'wsj_0006.pos', 'wsj_0007.pos', 'wsj_0009.pos',
-            'wsj_0013.pos', 'wsj_0014.pos', 'wsj_0027.pos', 'wsj_0055.pos', 'wsj_0067.pos', 
-            'wsj_0070.pos', 'wsj_0074.pos', 'wsj_0076.pos', 'wsj_0080.pos', 'wsj_0081.pos', 
-            'wsj_0084.pos', 'wsj_0096.pos', 'wsj_0115.pos', 'wsj_0131.pos', 'wsj_0147.pos',
-            'wsj_0153.pos', 'wsj_0154.pos']
+eval_set_pos = ['wsj_0023.pos', 'wsj_0054.pos', 'wsj_0057.pos', 'wsj_0063.pos', 
+                'wsj_0066.pos', 'wsj_0068.pos', 'wsj_0069.pos', 'wsj_0084.pos', 
+                'wsj_0124.pos', 'wsj_0197.pos']
 
-#grammar2 = "NP: {<PRP$>?<CD>?<DT>?<JJ.*>*<NN.*>*<POS>?<NN.*>*<JJ>?<CD>?}"    
 grammar = r"""
-    NP: {<NN.*><CD><JJ>?}
-        {<CD><NN.*>*<CD>?<JJ>?}
-        {<DT|PP\$>?<CD>?<VBG>?<JJ|RB>*<NN.*>+<VBG>?<JJ>*<NN.*>*}
-        }<CD><NNS><JJ>{
-        {<CD><NNS>}
-        }<NNS><JJ>{
-        {<NNS>}
-        {<WDT>}
+    NP: {<DT>?<NN.*>+<VBG><NN.*>+}
+        {<DT><CD>?<VBN><NN.*>*}
+        {<DT><CD><VBG>?<NN.*>+}
+        {<PDT>?<DT|WDT|PR.*>?<\$>?<CD>*<JJ.*>*<NN.*>*<POS>?<NN.*>*<\$>?<CD>*}
+        {<WP.*>}
+        {<EX>}  
 """
 
-chunks = nltk.corpus.treebank_chunk.chunked_sents(test_set)
-parser = nltk.RegexpParser(grammar)
+def create_parser(grammar):
+    return nltk.RegexpParser(grammar)
 
-index = 16
+def create_chunks_from_files(files):
+    return nltk.corpus.treebank_chunk.chunked_sents(files)
+   
+def evaluate_grammar(parser, chunks):
+    return parser.evaluate(chunks)   
+       
+def run():
+    parser = create_parser(grammar)
+    chunks = create_chunks_from_files(eval_set_pos)
+    print evaluate_grammar(parser, chunks)
 
-result = parser.parse(treebank.tagged_sents()[index])
-print "My parse: \n" + str(result) + "\n"
-print "Actual parse: \n" + str(nltk.corpus.treebank_chunk.chunked_sents()[index])
-
-print parser.evaluate(chunks)
+run()
